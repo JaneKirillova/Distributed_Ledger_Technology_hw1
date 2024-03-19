50d425979831daa85d4047ba97833777d768a04e
JaneKirillova <grindevald666@gmail.com>
2024-03-19 17:29:04 +0100

    Add parsing command line arguments

--- README.md
+++ README.md
@@ -4,3 +4,16 @@ Before usage please install all required libraries by running the following comm
 
 `pip install -r requirements.txt`
 
+The code allows to do two type of actions:
+
+1. Sign some message. To do it please run the following in the command line from the root directory:
+
+`python main.py "Your message to sign here" sign`
+
+After this command signed message will be printed.
+
+2. Verify that signature on the message. To do this please run the following in the command line from the root directory:
+
+`python main.py "Your signed message here" verify`
+
+This will print false or positive result of the verification
--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 from digital_signature import sign, verify, generate_keys
+import argparse
 
-if __name__ == "__main__":
+
+def main():
+    parser = argparse.ArgumentParser(description="Digital Signature Tool")
+    parser.add_argument("input", help="Message that you want to sign or verify", type=str)
+    parser.add_argument("action", help="Action to perform: 'verify' or 'sign' the message", choices=["verify", "sign"])
+
+    args = parser.parse_args()
+
+    message = args.input
+    action = args.action
     public_key, private_key = generate_keys()
-
-    message = "Message to sign."
-
     signature = sign(message, private_key)
 
-    is_verified = verify(message, signature, public_key)
-    print("Signature Verified:", is_verified)
+    if action == "verify":
+        is_verified = verify(message, signature, public_key)
+        print("Signature Verified:", is_verified)
+    elif action == "sign":
+        print("Sign message:", signature)
+
+
+if __name__ == "__main__":
+    main()
